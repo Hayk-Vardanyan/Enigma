893b30034d08e96e9547a03e8e5917b814c5c81e
HaykVardanyan2004 <90803123+HaykVardanyan2004@users.noreply.github.com>
2021-12-26 23:40:06 +0400

    Add files via upload

---
+++ cub2.h
@@ -0,0 +1,38 @@
+#ifndef CUBE_H
+#define CUBE_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+namespace Constants {
+
+ const char up = 'U';
+ const char down = 'D';
+ const char right = 'R';
+ const char left = 'L';
+ const int vertex = 8;
+
+}
+
+
+
+class Cube {
+    
+public:
+    void incode(std::string input_str);
+    std::string code();
+    std::string decode(const std::string & key);
+
+private:
+    void rotate_up(int current_cube); 
+    void rotate_down(int current_cube);
+    void rotate_left(int current_cube);
+    void rotate_right(int current_cube);
+
+private:
+    std::vector<std::string> separator;
+};
+
+#endif //CUBE_H
---
+++ cube.cpp
@@ -0,0 +1,145 @@
+#include "cub2.h"
+
+void Cube::incode (std::string input_str) {
+    
+    std::string tmp;
+    for(int i = 0; i < input_str.size(); ++i)
+    {
+        separator.push_back(input_str.substr(0, Constants::vertex));
+        input_str.erase(0, 8);
+    }
+    separator.push_back(input_str);
+}
+
+std::string Cube::code() {
+     
+    std::string tmp;
+    for(int i = 0; i < separator.size(); ++i) {
+        rotate_left(i);
+        rotate_up(i);
+        tmp += std::to_string(i) + "D:R ";
+    } 
+   std::cout << "HERE IS YOUR ENCRYPED TEXT -> ";
+   for(int i = 0; i < separator.size(); ++i){
+        std::cout << separator[i];
+    }
+    std::cout << '\n';
+    return tmp;
+}   
+
+std::string Cube::decode(const std::string& key) {
+    
+    std::string return_value;
+    int index = 0;
+    for(int i = 0; i < key.size(); ++i) {
+      
+
+    if(key[i]  >= '0' && key[i]  <= '9')
+    {
+        index = key[i] - '0';  // "casting" char to int 
+    }
+
+    else if(key[i] == Constants::down)
+    {
+        rotate_down(index);
+    }
+
+    else if(key[i] == Constants::up)
+    {
+        rotate_up(index);
+    }
+       
+        else if(key[i] == Constants::right)
+    {
+        rotate_right(index);
+    }
+
+        else if(key[i] == Constants::left)
+    {
+        rotate_left(index);
+    }
+    
+
+    }  
+
+    
+    std::cout << "HERS IS YOUR DECODED TEXT ->  ";
+
+    for(int i = 0; i < separator.size(); ++i) {
+        return_value += separator[i];
+        std::cout << separator[i];
+    }
+   
+    
+
+
+    return return_value;
+
+}
+
+void Cube::rotate_left(int current_cube) {
+    
+    char tmp = separator[current_cube][0];
+    char tmpp = separator[current_cube][3];
+    separator[current_cube][0] = separator[current_cube][1];
+    separator[current_cube][1] = separator[current_cube][5];
+    separator[current_cube][5] = separator[current_cube][4];
+    separator[current_cube][4] = tmp;
+    separator[current_cube][3] = separator[current_cube][2];
+    separator[current_cube][2] = separator[current_cube][6];
+    separator[current_cube][6] = separator[current_cube][7];
+    separator[current_cube][7] = tmpp;
+}
+
+void Cube::rotate_right(int current_cube) {
+    
+    char tmp = separator[current_cube][0];
+    char tmpp = separator[current_cube][3];
+    separator[current_cube][0] = separator[current_cube][4];
+    separator[current_cube][4] = separator[current_cube][5];
+    separator[current_cube][5] = separator[current_cube][1];
+    separator[current_cube][1] = tmp;
+    separator[current_cube][3] = separator[current_cube][7];
+    separator[current_cube][7] = separator[current_cube][6];
+    separator[current_cube][6] = separator[current_cube][2];
+    separator[current_cube][2] = tmpp;
+}
+
+void Cube::rotate_up(int current_cube) {
+    
+    char tmp = separator[current_cube][0];
+    char tmpp = separator[current_cube][1];
+    separator[current_cube][0] = separator[current_cube][3];
+    separator[current_cube][3] = separator[current_cube][7];
+    separator[current_cube][7] = separator[current_cube][4];
+    separator[current_cube][4] = tmp;
+    separator[current_cube][1] = separator[current_cube][2];
+    separator[current_cube][2] = separator[current_cube][6];
+    separator[current_cube][6] = separator[current_cube][5];
+    separator[current_cube][5] = tmpp;
+}
+
+void Cube::rotate_down(int current_cube) {
+    
+    char tmp = separator[current_cube][0];
+    char tmpp = separator[current_cube][1];
+    separator[current_cube][0] = separator[current_cube][4];
+    separator[current_cube][4] = separator[current_cube][7];
+    separator[current_cube][7] = separator[current_cube][3];
+    separator[current_cube][3] = tmp;
+    separator[current_cube][1] = separator[current_cube][5];
+    separator[current_cube][5] = separator[current_cube][6];
+    separator[current_cube][6] = separator[current_cube][2];
+    separator[current_cube][2] = tmpp;
+} 
+
+
+int main()
+{
+   Cube c;
+   c.incode("New Year");
+   c.decode(c.code());
+}
+
+
+
